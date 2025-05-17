ecf965dfb3b980e8d787a01a973cf63b5f653d45
Johnny Coral <coral.johnny.2@gmail.com>
2025-05-17 18:08:03 -0500

    Added pyscopg installation and dependecies to dockerfile

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Tests functionality of calc module"""
 
@@ -16,7 +17,9 @@ class CalcTests(SimpleTestCase):
     def test_substract_numbers(self):
         res = calc.sub(5, 2)
         self.assertEqual(res, 3)
-    
+        
     def test_multiply_numbers(self):
         res = calc.multiply(7, 2)
         self.assertEqual(res, 14)
+
+    
